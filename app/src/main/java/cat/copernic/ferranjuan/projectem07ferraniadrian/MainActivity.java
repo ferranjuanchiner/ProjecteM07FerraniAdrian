6b7a13d4e156d9d60ceef4a78ead6656ce1e09d4
charliecaufield <charliecaufield@gmail.com>
2019-12-11 11:02:53 +0100

    Intent login -> register

--- app/src/main/java/cat/copernic/ferranjuan/projectem07ferraniadrian/MainActivity.java
+++ app/src/main/java/cat/copernic/ferranjuan/projectem07ferraniadrian/MainActivity.java
@@ -2,13 +2,21 @@ package cat.copernic.ferranjuan.projectem07ferraniadrian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int TEXT_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchRegister(View view) {
+        Intent intent = new Intent(this, Register.class);
+        intent.putExtra("VAR INT", 1);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
